0b0cd6d5461610e3847468ada1c23abe3eafa5bd
Samir HACHIMI <hachimi.samir@gmail.com>
2016-09-06 13:59:02 +0200

    tempo z.h improvement

--- src/check/z.c
+++ src/check/z.c
@@ -1,19 +1,17 @@
 #include "z.h"
 
-#include <vector>
-
 std::vector<z_test_t> groups = std::vector<z_test_t>();
 
-bool test_bidon(void)
+static bool test_bidon(void)
 {
     Z_ASSERT(42 == 69);
-    
+
     return true;
 }
 
-void init(void) {
-    Z_GROUPS("echec", test_bidon);
-    
+Z_GROUP(test, "test the Z framework")
+{
+    Z_TEST("echec", test_bidon);
 }
 
 int main(int argc, char **argv)
@@ -21,10 +19,10 @@ int main(int argc, char **argv)
     int i = 0;
     int nb_failed = 0, nb_success = 0;
 
-    init();
+    init_test();
 
     while (i < groups.size()) {
-        std::cout << "Test " << groups[i].name;
+        std::cout << "Test `" << groups[i].name << "`:";
         if (groups[i].test_cb()) {
             std::cout << " Ok" << std::endl;
             nb_success++;
--- src/check/z.h
+++ src/check/z.h
@@ -2,22 +2,27 @@
 # define Z_H_
 
 # include <iostream>
+# include <vector>
 
 typedef struct {
     std::string name;
     bool (*test_cb)(void);
 } z_test_t;
 
-# define  Z_ASSERT(expr)                                                    \
-    if (!(expr)) {                                                          \
-        std::cerr << "Test " << __func__ << ": " #expr " failed at line "   \
-                  << __LINE__ << std::endl;                                 \
-        return false;                                                       \
-    }                                                                       \
+extern std::vector<z_test_t> groups;
 
-#define Z_GROUPS(_name, _cb)  groups.push_back((z_test_t){       \
-       .name = _name, .test_cb = _cb                             \
+# define  Z_ASSERT(expr)                                                     \
+    if (!(expr)) {                                                           \
+        std::cerr << "Test " << __func__ << ": " #expr " failed at line "    \
+                  << __LINE__ << std::endl;                                  \
+        return false;                                                        \
+    }
+
+# define Z_TEST(_name, _cb)  groups.push_back((z_test_t){                    \
+        .name = _name, .test_cb = _cb                                        \
    })
 
+# define Z_GROUP(_name, _desc)                                               \
+    static void init_##_name(std::string desc=_desc)
 
 #endif
